4f69e25ea604b25632f341e29e8517af0f99d881
xjeansantana <51293417+xjeansantana@users.noreply.github.com>
2022-03-23 18:06:07 -0300

    Alteração na estrutura de pastas

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const config = require ('config')
+const config = require('config')
 
 app.use(bodyParser.json())
 
-app.listen(config.get('api.porta'), () => console.log('API está rodando'))
+const roteador = require('./rotas/fornecedores')
+app.use('/api/fornecedores', roteador)
+
+app.listen(config.get('api.porta'), () => console.log('A API está funcionando!'))
