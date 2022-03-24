1a7278d047f781e1c70e9936677aefa50ab53176
xjeansantana <51293417+xjeansantana@users.noreply.github.com>
2022-03-24 11:27:44 -0300

    Aprofundando método get

--- api/rotas/fornecedores/TabelaFornecedor.js
+++ api/rotas/fornecedores/TabelaFornecedor.js
@@ -6,5 +6,27 @@ module.exports = {
     },
     inserir(fornecedor) {
         return Modelo.create(fornecedor)
+    },
+    async pegarPorId(id) {
+        const encontrado = await Modelo.findOne({
+            where: {
+                id: id
+            }
+        })
+
+        if (!encontrado) {
+            throw new Error('Fornecedor não encontrado')
+        }
+
+        return encontrado
+    },
+    atualizar(id, dadosParaAtualizar) {
+        return Modelo.update(
+            dadosParaAtualizar, {
+                where: {
+                    id: id
+                }
+            }
+        )
     }
 }
