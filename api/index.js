3b33b35ce2201b0274d7cbff5ccc029697b6df55
xjeansantana <51293417+xjeansantana@users.noreply.github.com>
2022-03-26 10:01:39 -0300

    Validando com antecidência

--- api/index.js
+++ api/index.js
@@ -3,19 +3,49 @@ const app = express()
 const bodyParser = require('body-parser')
 const config = require('config')
 const NaoEncontrado = require('./erros/NaoEncontrado')
+const CampoInvalido = require('./erros/CampoInvalido')
+const DadosNaoFornecidos = require('./erros/DadosNaoFornecidos')
+const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
+const formatosAceitos = require('./Serializador').formatosAceitos
 
 app.use(bodyParser.json())
+
+app.use((requisicao, resposta, proximo) => {
+    let formatoRequisitado = requisicao.header('Accept')
+
+    if (formatoRequisitado === '*/*') {
+        formatoRequisitado = 'application/json'
+    }
+
+    if (formatosAceitos.indexOf(formatoRequisitado) === -1) {
+        resposta.status(406)
+        resposta.end()
+        return
+    }
+
+    resposta.setHeader('Content-Type', formatoRequisitado)
+    proximo()
+})
 
 const roteador = require('./rotas/fornecedores')
 app.use('/api/fornecedores', roteador)
 
 app.use((erro, requisicao, resposta, proximo) => {
+    let status = 500
+
     if (erro instanceof NaoEncontrado) {
-        resposta.status(404)
-    } else {
-        resposta.status(400)
+        status = 404
     }
 
+    if (erro instanceof CampoInvalido || erro instanceof DadosNaoFornecidos) {
+        status = 400
+    }
+
+    if (erro instanceof ValorNaoSuportado) {
+        status = 406
+    }
+
+    resposta.status(status)
     resposta.send(
         JSON.stringify({
             mensagem: erro.message,
--- banco-de-dados/criarTabelas.js
+++
@@ -1,6 +0,0 @@
-const ModeloTabela = require('../api/rotas/fornecedores/ModeloTabelaFornecedor')
-
-ModeloTabela
-    .sync()
-    .then(() => console.log('Tabela criada com sucesso'))
-    .catch(console.log)
